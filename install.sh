1cf2f49897301c5372cf26e35060a165627bfa1c
zzz00 <zzz00.hana@gmail.com>
2025-01-19 11:16:22 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Configuration
+GH_REPO_USER="zzz-00"
+GH_REPO_NAME="dotfiles"
+GH_BRANCH="main"
+DOT_DEST_DIR="${HOME}/dotfiles"
+
+GH_REPO_URL="https://github.com/${GH_REPO_USER}/${GH_REPO_NAME}"
+GH_REPO_SSH="git@github.com:${GH_REPO_USER}/${GH_REPO_NAME}"
