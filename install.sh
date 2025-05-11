cd3a4be4f41c26282b19fa48ed3ff4019d29a696
zzz00 <zzz00.hana@gmail.com>
2025-05-11 12:50:37 +0800

    Add configuration files for tofi, waybar, and yazi
    - Created default configuration for tofi with extensive theming options including fonts, text colors, and window behavior.
    - Added waybar configuration for top and upper bar with modules for window management, workspaces, system stats, and a clock.
    - Implemented CSS styles for waybar to enhance visual appearance and transitions.
    - Introduced yazi configuration for file management with options to show hidden files and edit with vim.

--- install.sh
+++ install.sh
@@ -8,3 +8,12 @@ DOT_DEST_DIR="${HOME}/dotfiles"
 
 GH_REPO_URL="https://github.com/${GH_REPO_USER}/${GH_REPO_NAME}"
 GH_REPO_SSH="git@github.com:${GH_REPO_USER}/${GH_REPO_NAME}"
+ZIP_URL="${GH_REPO_URL}/archive/refs/heads/${GH_BRANCH}.zip"
+TMP_ZIP="dotfiles.tmp.zip"
+TMP_ZIP_ROOT_DIR="${GH_REPO_NAME}-${GH_BRANCH}"
+
+OG_PWD="${PWD}"
+TMP_DIR='mktemp -d'
+
+echo "dotfiles installer for arch"
+
